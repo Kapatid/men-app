dda36abb2a2a4d39abfec75ec788d3357740d9dd
Nadji <nadjitan@yahoo.com.ph>
2021-08-04 22:58:01 +0800

    Add dedicated route codes and more refactor

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,35 @@
-import { connectDatabase } from "./db/connectDatabase";
-import UserController from './controllers/UserController'
+import UserRoutes from './routes/UserRoutes';
 
 import express, { Application } from 'express';
 import morgan from 'morgan';
+import mongoose, { CallbackError } from 'mongoose';
 import { json, urlencoded } from 'body-parser';
 
 
 const app: Application = express();
-const port: string | number = process.env.PORT || 3000;
+const PORT: string | number = process.env.PORT || 3000;
 
-app
-  .use(json()) // for parsing application/json
-  .use(urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded;
-  .use(morgan('dev'))
-  .use('/users', UserController);
+app.use(json()) // for parsing application/json
+   .use(urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded;
+   .use(morgan('dev'))
+   .use('/users', UserRoutes);
 
-(async () => {
-  await connectDatabase();
-})();
+mongoose.connect(
+  process.env.MONGODB_URI as string, 
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true
+  }, 
+  (error: CallbackError) => {
+    if (!error) {
+      console.log('\x1b[1m\x1b[33m' + 'Database Connected!' + '\x1b[0m');
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+      app.listen(PORT, () => {
+        (PORT === 3000) && console.log(`Server running in: http://localhost:${PORT}`);
+      });
+    }
+
+    else console.log('\x1b[1m\x1b[31m' + '[ERROR] '+ '\x1b[0m' + error.message);
+  }
+);
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,82 +1,63 @@
 import User, { IUser } from '../models/User';
 
-import express, { Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { CallbackError, isValidObjectId } from 'mongoose';
 
-const router = express.Router();
+export const getUsers = async(req: Request, res: Response) => {
+  await User.find((error: CallbackError, docs: IUser) => {
+    handleUserError(error, docs, res, 500);
+  });
+}
 
-// GET Users
-router.get('/', 
-  async (req: Request, res: Response) => {
-
-    await User.find((error: CallbackError, docs: IUser) => {
-      handleUserError(error, docs, res, 500);
-    });
+export const getUser = async(req: Request, res: Response) => {
+  if (idExists(req, res) === true) {
+    await User.findById(req.params.id, 
+      (error: CallbackError, docs: IUser) => {
+        handleUserError(error, docs, res, 500);
+      }
+    );
   }
-);
+}
 
-// GET User by id
-router.get('/:id', 
-  async (req: Request, res: Response) => {
+export const createUser = async(req: Request, res: Response) => {
+  const user: IUser = new User({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email
+  });
 
-    if (idExists(req, res) === true) {
-      await User.findById(req.params.id, 
-        (error: CallbackError, docs: IUser) => {
-          handleUserError(error, docs, res, 500);
-      });
-    }
+  user.save((error: CallbackError, docs: IUser) => {
+    handleUserError(error, docs, res, 500);
+  }); 
+}
+
+export const updateUser = async(req: Request, res: Response) => {
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email
+  };
+
+  if (idExists(req, res) === true) {
+    await User.findByIdAndUpdate(req.params.id,
+      { $set: user }, 
+      (error: CallbackError, docs: IUser | null) => {
+        handleUserError(error, docs, res, 500);
+      }
+    );
   }
-);
+}
 
-// CREATE New User
-router.post('/', 
-  async (req: Request, res: Response) => {
-
-    const user: IUser = new User({
-      username: req.body.username,
-      password: req.body.password,
-      email: req.body.email
-    });
-
-    user.save((error: CallbackError, docs: IUser) => {
-      handleUserError(error, docs, res, 500);
-    }); 
+export const deleteUser = async(req: Request, res: Response) => {
+  if (idExists(req, res) === true) {
+    await User.findByIdAndDelete(req.params.id, 
+      {}, 
+      (error: CallbackError, docs: IUser | null) => {
+        handleUserError(error, docs, res, 500);
+      }
+    );
   }
-);
-
-// UPDATE User
-router.put('/:id', 
-  async (req: Request, res: Response) => {
-
-    const user = {
-      username: req.body.username,
-      password: req.body.password,
-      email: req.body.email
-    };
-
-    if (idExists(req, res) === true) {
-      await User.findByIdAndUpdate(req.params.id,
-        { $set: user }, 
-        (error: CallbackError, docs: IUser | null) => {
-          handleUserError(error, docs, res, 500);
-        });
-    }
-  }
-);
-
-// DELETE User
-router.delete('/:id', 
-  async (req: Request, res: Response) => {
-
-    if (idExists(req, res) === true) {
-      await User.findByIdAndDelete(req.params.id, 
-        {}, 
-        (error: CallbackError, docs: IUser | null) => {
-          handleUserError(error, docs, res, 500);
-      });
-    }
-  }
-);
+}
 
 function idExists(req: Request, res: Response): boolean | Response {
   if (isValidObjectId(req.params.id)) return true;
@@ -96,5 +77,3 @@ function handleUserError(
 
   else res.status(status).send(`[ERROR] ${error.message}`);
 }
-
-export default router;
